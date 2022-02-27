24aba1057479295dcbcf0a60c2edfb2b66f2153d
Sheps <thesheps@gmail.com>
2022-02-27 09:58:11 +0000

    feat: adds style override to NavBar

--- src/nav-bar/index.js
+++ src/nav-bar/index.js
@@ -1,6 +1,12 @@
-import { html, LitElement } from "https://unpkg.com/lit?module";
+import { html, css, LitElement } from "https://unpkg.com/lit?module";
 
 class NavBar extends LitElement {
+	static styles = css`
+		li {
+			list-style: none;
+		}
+	`;
+
 	render() {
 		return html`<nav id="nav-bar">
 			<ul>
