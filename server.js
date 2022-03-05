c84f4519309d13d442a488ffd2ead04f7ce18964
Sheps <thesheps@gmail.com>
2022-03-05 08:02:44 +0000

    test: runs tests on 9090 so we can host and test at the same time

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("live-server").start({
 	file: "index.html",
 	open: false,
-	port: process.env.PORT ?? "9090",
+	port: process.env.NODE_ENV ? 9090 : process.env.PORT,
 	root: "src",
 });
--- tests/testData.js
+++ tests/testData.js
@@ -1,6 +1,6 @@
 export const expectedUsername = "my-lovely-username";
 export const expectedRepo = { name: "my-lovely-repo" };
 export const expectedBranch = { name: "my-lovely-branch" };
-export const baseUrl = `http://localhost:${process.env.PORT}`;
+export const baseUrl = `http://localhost:9090`;
 export const repoUrl = `${baseUrl}/${expectedUsername}/${expectedRepo.name}`;
 export const branchesUrl = `${repoUrl}}/branches`;
