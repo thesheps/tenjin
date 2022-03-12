1bd24d82de0356b28c4e7f5c2331ad2e77d337cd
Sheps <thesheps@gmail.com>
2022-03-12 16:49:54 +0000

    feat: adds axios to api to obtain creds

--- api/src/handler.js
+++ api/src/handler.js
@@ -1,10 +1,26 @@
 "use strict";
 
+const axios = require("axios");
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
 module.exports.auth = async (event) => {
 	const code = event.queryStringParameters["code"];
 
+	const params = {
+		client_id: CLIENT_ID,
+		client_secret: CLIENT_SECRET,
+		code,
+	};
+
+	const output = await axios.get(
+		"https://github.com/login/oauth/access_token?",
+		{ params }
+	);
+
 	return {
-		body: JSON.stringify({ accessToken: code }),
+		body: JSON.stringify({ accessToken: output }),
 		statusCode: 200,
 		headers: {
 			"Access-Control-Allow-Origin": "*",
