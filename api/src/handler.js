a34e1f8049e502177c49f38d24a09ed81f1f4031
Sheps <thesheps@gmail.com>
2022-03-12 18:38:47 +0000

    feat(api): constructs json response from github auth respone

--- api/src/handler.js
+++ api/src/handler.js
@@ -14,18 +14,22 @@ module.exports.auth = async (event) => {
 	const params = { client_id, client_secret, code };
 
 	try {
-		const response = await axios.post(
+		const response = await axios.get(
 			"https://github.com/login/oauth/access_token?",
-			null,
 			{ params }
 		);
 
-		const accessToken = response;
-		console.log(accessToken);
+		const body = {};
+		const vals = response.data.split("&");
+
+		for (let i = 0; i < vals.length; i++) {
+			const kvp = vals.split("=");
+			body[kvp[0]] = kvp[1];
+		}
 
 		return {
 			...headers,
-			body: JSON.stringify(accessToken),
+			body: JSON.stringify(body),
 			statusCode: 200,
 		};
 	} catch (e) {
