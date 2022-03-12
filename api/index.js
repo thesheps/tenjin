29c1cc473f805d6eb48079a66b2ae30173e9414a
Sheps <thesheps@gmail.com>
2022-03-12 17:22:40 +0000

    feat: fixes deserialisation of auth response

---
+++ api/index.js
@@ -0,0 +1,9 @@
+const { auth } = require("./src/handler");
+
+auth({
+	queryStringParameters: {
+		code: "beans",
+	},
+}).then((data) => {
+	console.log(data.body);
+});
--- api/src/handler.js
+++ api/src/handler.js
@@ -2,31 +2,35 @@
 
 const axios = require("axios");
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const headers = {
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Credentials": true,
+};
 
 module.exports.auth = async (event) => {
 	const code = event.queryStringParameters["code"];
+	const params = { client_id, client_secret, code };
 
-	const params = {
-		client_id: CLIENT_ID,
-		client_secret: CLIENT_SECRET,
-		code,
-	};
+	try {
+		const response = await axios.get(
+			"https://github.com/login/oauth/access_token?",
+			{ params }
+		);
 
-	const response = await axios.get(
-		"https://github.com/login/oauth/access_token?",
-		{ params }
-	);
+		const accessToken = response.data;
 
-	const accessToken = response.json();
-
-	return {
-		body: JSON.stringify(accessToken),
-		statusCode: 200,
-		headers: {
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Credentials": true,
-		},
-	};
+		return {
+			...headers,
+			body: JSON.stringify(accessToken),
+			statusCode: 200,
+		};
+	} catch (e) {
+		return {
+			...headers,
+			body: "Couldn't obtain access token!",
+			statusCode: 403,
+		};
+	}
 };
