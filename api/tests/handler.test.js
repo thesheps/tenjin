251422ea2d48012de23b20ec4e13f181a808ff01
Sheps <thesheps@gmail.com>
2022-03-13 08:49:56 +0000

    test: adds negative test

--- api/tests/handler.test.js
+++ api/tests/handler.test.js
@@ -1,16 +1,19 @@
 const axios = require("axios");
+const { when } = require("jest-when");
 const { auth } = require("../src/handler");
 
 process.env.CLIENT_ID = "CLIENT_ID";
 process.env.CLIENT_SECRET = "CLIENT_SECRET";
 
 const testEvent = { queryStringParameters: { code: "foobarbaz" } };
+const testNaughtyEvent = { queryStringParameters: { code: "iamverynaughty" } };
+
 const expectedUrl = "https://github.com/login/oauth/access_token?";
 const expectedParams = {
 	params: {
 		client_id: process.env.CLIENT_ID,
 		client_secret: process.env.CLIENT_SECRET,
-		code: "foobarbaz",
+		code: testEvent.queryStringParameters.code,
 	},
 };
 
@@ -23,15 +26,45 @@ const expectedOutput = {
 	statusCode: 200,
 };
 
-jest.mock("axios", () => ({
-	get: jest.fn().mockReturnValue(Promise.resolve({ data: "accessToken=qux" })),
-}));
+const naughtyParams = {
+	params: {
+		...expectedParams.params,
+		code: testNaughtyEvent.queryStringParameters.code,
+	},
+};
+
+const naughtyOutput = {
+	...expectedOutput,
+	body: "Couldn't obtain access token!",
+	statusCode: 403,
+};
+
+when(axios.get)
+	.calledWith(expectedUrl, expectedParams)
+	.mockReturnValue(Promise.resolve({ data: "accessToken=qux" }));
+
+when(axios.get)
+	.calledWith(expectedUrl, naughtyParams)
+	.mockReturnValue(Promise.resolve("Badly formatted code string! Stop it!!"));
+
+jest.mock("axios");
 
 describe("Auth Handler", () => {
+	afterEach(() => {
+		jest.resetAllMocks();
+	});
+
 	it("Returns a valid access token using the correct client details", async () => {
 		const output = await auth(testEvent);
 
 		expect(axios.get).toHaveBeenCalledWith(expectedUrl, expectedParams);
 		expect(output).toEqual(expectedOutput);
+	});
+
+	it("Returns a well-formed error when the gihub api call can't be made", async () => {
+		const output = await auth(testNaughtyEvent);
+
+		expect(axios.get).toHaveBeenCalledWith(expectedUrl, naughtyParams);
+		expect(output).toEqual(naughtyOutput);
 	});
 });
