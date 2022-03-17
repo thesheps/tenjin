fea1ee3c4c22fb854e543436be5b6ad091189a51
Sheps <thesheps@gmail.com>
2022-03-17 11:13:12 +0000

    feat: filters out any non-markdown files

--- app/tests/components/file-lister/get-files.test.js
+++ app/tests/components/file-lister/get-files.test.js
@@ -1,22 +1,27 @@
 import getFiles from "../../../src/components/file-lister/get-files";
 
-const expectedFiles = [{ path: "foo" }];
+const expectedFiles = [{ path: "foo" }, { path: "bar" }, { path: "baz.md" }];
 const json = jest.fn().mockReturnValue({ tree: expectedFiles });
 global.fetch = jest.fn(() => Promise.resolve({ json }));
 
 describe("Get Files", () => {
-	it("Calls fetch with expected URL", async () => {
-		const expectedAccount = "foobar";
-		const expectedRepo = "baz";
-		const expectedBranch = "baz";
-		const expectedToken = "qux";
-		const files = await getFiles(
+	const expectedAccount = "foobar";
+	const expectedRepo = "baz";
+	const expectedBranch = "baz";
+	const expectedToken = "qux";
+
+	let files = [];
+
+	beforeAll(async () => {
+		files = await getFiles(
 			expectedAccount,
 			expectedRepo,
 			expectedBranch,
 			expectedToken
 		);
+	});
 
+	it("Calls fetch with expected URL", async () => {
 		expect(global.fetch).toHaveBeenCalledWith(
 			`https://api.github.com/repos/${expectedAccount}/${expectedRepo}/git/trees/${expectedBranch}?recursive=1`,
 			{
@@ -25,7 +30,9 @@ describe("Get Files", () => {
 				},
 			}
 		);
+	});
 
-		expect(files).toEqual(["foo"]);
+	it("Filters out any non-markdown files", async () => {
+		expect(files).toEqual(["baz.md"]);
 	});
 });
